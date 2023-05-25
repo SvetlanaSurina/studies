99318254c74dcd0074c4e7b00daefc5df01d2a2a
Svetlana Surina <sveta170783@mail.ru>
2023-05-25 16:46:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Запускаем игру");
+        JFrame window = new JFrame("TicTacToe");
+        window.setSize(400, 400);
+        window.setLayout(new BorderLayout());
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        TicTacToe game = new TicTacToe();
+        window.add(game);
+        System.out.println("конец");
+    }
+}
